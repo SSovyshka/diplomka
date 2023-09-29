fba9c5037dbcb24fd1fca2dfc49c4fcef73debd3
BohdanSavchenkoo <aze.bog@gmail.com>
2023-09-29 19:53:00 +0300

    add smart enemy
    - navMeshes

--- sovyshkaGame/Assets/Scripts/EnemyController.cs
+++ sovyshkaGame/Assets/Scripts/EnemyController.cs
@@ -1,32 +1,23 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    Transform target;
+    [SerializeField] Transform target;
     NavMeshAgent agent;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
-
-        // Убедитесь, что у вас есть NavMesh на сцене, и враг может использовать его.
-        if (agent == null)
-        {
-            Debug.LogError("NavMeshAgent не найден на объекте врага.");
-        }
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Устанавливаем позицию назначения для NavMeshAgent как позицию игрока
-        if (target != null)
-        {
-            agent.SetDestination(target.position);
-        }
+        agent.SetDestination(target.position);
     }
+
 }
--- sovyshkaGame/Assets/Scripts/Guns/Weapons/Bullet.cs
+++ sovyshkaGame/Assets/Scripts/Guns/Weapons/Bullet.cs
@@ -49,6 +49,7 @@ public class Bullet : MonoBehaviour
         // Вычисляем угол поворота пули
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
         Invoke("DestroyTime", 3);
     }
 
